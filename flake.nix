42b41b5e5bed09cc96db0b5f40078b63e033ddb9
Lucie <gnosticstate@gmail.com>
2024-06-30 00:27:19 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    #       ↑ Swap it for your system if needed
+    #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        # standard toolkit
+        pkgs.nodejs # nixpkgs provides a "nodejs" package that corresponds to the current LTS version of nodejs, but you can specify a version (i.e node_20) if necessary
+        pkgs.yarn
+        pkgs.pnpm # a faster alternative to npm and yarn, with a less adopted toolchain
+
+        # optionally required by your code editor to lint and format your code
+        pkgs.nodePackages.prettier # formatter
+        pkgs.nodePackages.eslint # linter
+
+        # example package to serve a static nextjs export
+        pkgs.nodePackages.serve
+      ];
+      # ...
+    };
+  };
+}
